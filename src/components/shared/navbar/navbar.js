af53c1c78ebab6dea50d7eaefb9277ea59de4f9d
Krish2208 <diwankrish.17@gmail.com>
2020-06-19 12:38:55 +0530

    Notification Menu Added but some Styling Required

--- src/components/shared/navbar/navbar.js
+++ src/components/shared/navbar/navbar.js
@@ -19,7 +19,9 @@ import {
     PencilIcon,
     BriefcaseIcon,
     SignOutIcon,
-    ChevronRightIcon
+    ChevronRightIcon,
+    XIcon,
+    AlertIcon
 } from '@primer/octicons-react';
 import UserAvatar from '../../../assets/user.png';
 import './navbar.css';
@@ -28,10 +30,13 @@ class RenderNav extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            userProfileAnchor: null
+            userProfileAnchor: null,
+            notificationAnchor: null,
         };
         this.openUserProfile = this.openUserProfile.bind(this);
         this.closeUserProfile = this.closeUserProfile.bind(this);
+        this.openNotification = this.openNotification.bind(this);
+        this.closeNotification = this.closeNotification.bind(this);
     }
 
     /* To open the profile popup */
@@ -48,6 +53,18 @@ class RenderNav extends Component {
         });
     }
 
+    openNotification = (event) => {
+        this.setState({
+            notificationAnchor: event.currentTarget
+        });
+    }
+
+    closeNotification = (event) => {
+        this.setState({
+            notificationAnchor: null
+        });
+    }
+
     render() {
         return (
             <div className="navbar-main">
@@ -58,7 +75,7 @@ class RenderNav extends Component {
                                 Reallos
                             </Typography>
                             <div className="navbar-btn-group">
-                                <IconButton><BellIcon size={20}/></IconButton>
+                                <IconButton onClick={this.openNotification}><BellIcon size={20}/></IconButton>
                                 <IconButton><InboxIcon size={20}/></IconButton>
                                 <IconButton><ChecklistIcon size={20}/></IconButton>
                                 <IconButton onClick={this.openUserProfile}><Avatar src={UserAvatar} /></IconButton>
@@ -116,13 +133,13 @@ class RenderNav extends Component {
                             <Grid item>
                                 <Divider className="divider-profile"/>
                             </Grid>
-                            <Grid container className="profile-popup-action-group" direction="row" alignItems="flex-end" justify="space-evenly">
+                           <Grid container className="profile-popup-action-group" direction="row" alignItems="flex-end" justify="space-evenly">
                                 <Grid item>
                                     <Button>
                                         <Grid  container direction="column" justify="center" alignItems="center">
                                             <Grid item><PencilIcon /></Grid>
                                             <Grid item><Box component="p" className="profile-subheading-small">Edit Profile</Box></Grid>
-                                        </Grid>
+                                         </Grid>
                                     </Button>
                                 </Grid>
                                 <Grid item>
@@ -140,6 +157,107 @@ class RenderNav extends Component {
                                             <Grid item><Box component="p" className="profile-subheading-small">Log Out</Box></Grid>
                                         </Grid>
                                     </Button>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Menu>
+                    <Menu
+                        className="navbar-notification-popup"
+                        anchorEl={this.state.notificationAnchor}
+                        keepMounted
+                        open={Boolean(this.state.notificationAnchor)}
+                        onClose={this.closeNotification}
+                        getContentAnchorEl={null}
+                        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+                        transformOrigin={{ vertical: "top", horizontal: "center" }}
+                        PaperProps={{
+                            style: {
+                                borderRadius: 20
+                            }
+                        }}
+                    >
+                        <Grid container direction="column" justify="center" alignItems="center" className="notification-menu">
+                            <Grid item justify="center">
+                                <Box component="p" ml={5} style={{alignContent:"center"}}>
+                                    You have
+                                </Box>
+                                <Box component="p" mt={-2.5} mr={2} style={{fontWeight:800 , paddingTop: 1}}>
+                                    3 New Notifications
+                                </Box>
+                            </Grid>
+                            <Grid item style={{width: '100%', paddingLeft: 20, marginTop: 10}}>
+                                <Grid container direction="row" alignItems="center" spacing={1}>
+                                    <Grid item xs={2}>
+                                        <Avatar style={{width: 30 , height: 30}}>R</Avatar>
+                                    </Grid>
+                                    <Grid item xs={7}>
+                                        <Grid item justify="flex-start">
+                                            <Box component="p" style={{fontWeight:800 , paddingTop: 1}}>
+                                                You have
+                                            </Box>
+                                            <Box component="p" mt={-2.5} mr={2} style={{fontSize:12}}>
+                                                3 New Notifications
+                                            </Box>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid item xs={1}>
+                                        <IconButton>
+                                            <XIcon></XIcon>
+                                        </IconButton>
+                                    </Grid>
+                                </Grid>
+                                <Grid item>
+                                <   Divider variant='middle'/>
+                                </Grid>
+                            </Grid>
+                            <Grid item style={{width: '100%', paddingLeft: 20, marginTop: 10}}>
+                                <Grid container direction="row" alignItems="center" spacing={1}>
+                                    <Grid item xs={2}>
+                                        <Avatar style={{width: 30 , height: 30}}>R</Avatar>
+                                    </Grid>
+                                    <Grid item xs={7}>
+                                        <Grid item justify="flex-start">
+                                            <Box component="p" style={{fontWeight:800 , paddingTop: 1}}>
+                                                You have
+                                            </Box>
+                                            <Box component="p" mt={-2.5} mr={2} style={{fontSize:12}}>
+                                                3 New Notifications
+                                            </Box>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid item xs={1}>
+                                        <IconButton>
+                                            <XIcon></XIcon>
+                                        </IconButton>
+                                    </Grid>
+                                </Grid>
+                                <Grid item>
+                                <   Divider variant='middle'/>
+                                </Grid>
+                            </Grid>
+                            <Grid item style={{width: '100%', paddingLeft: 20, marginTop: 10}}>
+                                <Grid container direction="row" alignItems="center" spacing={1}>
+                                    <Grid item xs={2}>
+                                        <Avatar style={{width: 30 , height: 30}}><AlertIcon style={{backgroundColor:'yellow'}}/></Avatar>
+                                    </Grid>
+                                    <Grid item xs={7}>
+                                        <Grid item justify="flex-start">
+                                            <Box component="p" style={{fontWeight:800 , paddingTop: 1}}>
+                                                You have
+                                            </Box>
+                                            <Box component="p" mt={-2.5} mr={2} style={{fontSize:12}}>
+                                                3 New Notifications
+                                            </Box>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid item xs={1}>
+                                        <IconButton>
+                                            <XIcon></XIcon>
+                                        </IconButton>
+                                    </Grid>
+                                </Grid>
+                                <Grid item>
+                                <   Divider variant='middle'/>
                                 </Grid>
                             </Grid>
                         </Grid>
